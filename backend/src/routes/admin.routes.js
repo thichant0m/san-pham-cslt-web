6f7e3f1a1ed541a31ff0ea847ec99d5a64b8fa5b
thichant0m <113283229+thichant0m@users.noreply.github.com>
2026-05-18 08:39:47 +0700

    Delete backend directory

--- backend/src/routes/admin.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/auth.controller');
-
-router.post('/register', authController.register); // Đường dẫn Đăng ký
-router.post('/login', authController.login);       // Đường dẫn Đăng nhập
-
-module.exports = router;
